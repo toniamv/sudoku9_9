7c4db1f68715f888b79c7018250333703046d769
Gabriel Antonio <toni.gamv@gmail.com>
2025-05-15 12:57:36 -0300

    Implementando adicionar novo numero e base para avaliar fim de jogo - vidas adicionadas

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import edu.game.components.SudokuMatriz;
 
 public class Main {
     public static void main(String[] args) {
-        SudokuMatriz matriz = new SudokuMatriz(4);
-        matriz.printaMatriz();
+        SudokuMatriz matriz = new SudokuMatriz(2);
+        matriz.adicionarNumero(1, 0, 0);
+        matriz.adicionarNumero(2, 0, 1);
+        matriz.adicionarNumero(2, 0, 2);
+        matriz.adicionarNumero(2, 1, 0);
+        matriz.adicionarNumero(2, 1, 2);
     }
 }
--- src/edu/game/components/SudokuMatriz.java
+++ src/edu/game/components/SudokuMatriz.java
@@ -1,31 +1,34 @@
 package edu.game.components;
 
+
 public class SudokuMatriz {
     private Position[][] m_principal;
     private int size;
     private int fq;
+    private int lives = 3;
+    private boolean isGameOver = false;
 
     // Dificultade do jogo = nº de quadrantes pr linha (2,3,4)
     public SudokuMatriz(int dif){
         size = dif*3;
         fq = size/3;
         m_principal = new Position[size][size];
-
         for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j+=3){
+            for(int j = 0; j < size; j++){
                 m_principal[i][j] = new Position(-1);
-                m_principal[i][j+1] = new Position(-1);
-                m_principal[i][j+2] = new Position(-1);
             }
         }
+    }
+    public boolean acabouJogo(){
+        return isGameOver;
     }
 //Calcula quadrante ((int)i/3)*fq+(int)j/3
     public void printaMatriz(){
         for(int i = 0; i < size; i++){
             for(int j = 0; j < size; j+=3){
                 System.out.print(m_principal[i][j].getValue() + " ");
-                System.out.print(m_principal[i][j].getValue() + " ");
-                System.out.print(m_principal[i][j].getValue());
+                System.out.print(m_principal[i][j+1].getValue() + " ");
+                System.out.print(m_principal[i][j+2].getValue());
 
                 if(j+3 < size)System.out.print("|");
                 else System.out.println();
@@ -34,30 +37,51 @@ public class SudokuMatriz {
         }
     }
 
-//    public int adicionarNumero(int value, int x, int y){
-//        if(avaliarPosicao()){
-//            m_principal[x][y].setValue(value);
-//            return 0;
-//        }
-//        if(avaliarJogo()) return 1;
-//        System.out.println("Não é recomendado adicionar " + value + " aqui");
-//        return -1;
-//    }
-//
-//    private int getQuadrante(int x, int y){
-//        return ((int)x/3)*fq+(int)y/3;
-//    }
-//
-//    private boolean avaliarPosicao(int value){
-//        if()
-//    }
+    public int adicionarNumero(int value, int x, int y){
+        if(avaliarPosicao(value, x, y)){
+            m_principal[x][y].setValue(value);
+            printaMatriz();
+            return 0;
+        }
+        if(avaliarJogo()) return 1;
+        System.out.println("Não é recomendado adicionar " + value + " aqui");
+        lives = lives-1;
+        printaMatriz();
+        return -1;
+    }
+
+    private int getQuadrante(int x, int y){
+        return (x/3)*fq+y/3;
+    }
+
+    private boolean avaliarPosicao(int value, int x, int y){
+        for(int i = 0; i < size; i++){
+            if(m_principal[x][i].getValue() == value) return false;
+            if(m_principal[i][y].getValue() == value) return false;
+        }
+        return avaliaQuadrante(getQuadrante(x,y), value);
+    }
 
     private boolean avaliaQuadrante(int quadrante, int value){
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
-                if(m_principal[i+3*((int)quadrante/fq)][j+3*(quadrante%fq)].getValue() == value){
+                if(m_principal[i+3*(quadrante/fq)][j+3*(quadrante%fq)].getValue() == value){
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+
+    private boolean avaliarJogo(){
+        if(lives == 0){
+            System.out.println("Game Over!");
+            isGameOver = true;
+            return false;
+        }
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(m_principal[i][j].getValue() == -1) return false;
             }
         }
         return true;
